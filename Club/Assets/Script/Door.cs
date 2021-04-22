1bf2f4843d91ee3a414049207cc053b5421ade0c
alexey231090 <alexey231090@gmail.com>
2021-04-22 22:59:51 +0700

    Add Door
    Add door the game

---
+++ Club/Assets/Script/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+   public bool OpenClose = false;
+    void Start()
+    {
+         bool OpenClose = false;
+}
+
+   
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) // Вход или касание 
+    {
+
+        if (other.tag == "Player")
+        {
+            OpenClose = true;
+        }
+        else if(other.tag != "Player")
+
+        {
+            OpenClose = false;
+        }
+    }
+
+  
+}
---
+++ Club/Assets/Script/raycast.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    public GameObject obj;
+    Vector3 Doorposition;
+    public Door door;
+    void Start()
+    {
+       Doorposition = obj.transform.position;
+      
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward); 
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.yellow);
+        print(door.OpenClose);
+
+
+        if (Physics.Raycast(ray) && door.OpenClose == true && Input.GetKeyDown(KeyCode.E))
+        {
+
+            obj.transform.position = new Vector3(Doorposition.x, Doorposition.y, -8.25f);
+
+        }
+        else if (!Physics.Raycast(ray))
+        {
+            return;
+        }
+            
+       
+    }
+
+    
+
+}
